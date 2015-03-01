b4127d58998ab4669edc9ba9cef62dbaee18c607
Martin Dilling-Hansen <martindilling@gmail.com>
2015-03-01 14:22:51 +0100

    Tests refactored

--- tests/BishNishTest.php
+++ tests/BishNishTest.php
@@ -7,80 +7,94 @@ class BishNishTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_should_return_empty_array_given_empty_array()
     {
-        $rules = [];
-        $data = [];
-        $bishNish = new BishNish($data, $rules);
+        $data     = [];
+        $rules    = [];
+        $result   = [];
 
-        $this->assertEquals([], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_should_return_data_with_no_rules_given()
     {
-        $rules = [];
-        $data = ['A', 'B', 'C'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['A', 'B', 'C'];
+        $rules    = [];
+        $result   = ['A', 'B', 'C'];
 
-        $this->assertEquals(['A', 'B', 'C'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_can_use_a_rule()
     {
-        $rules = ['B' => 'Bish'];
-        $data = ['A', 'B', 'C'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['A', 'B', 'C'];
+        $rules    = ['B' => 'Bish'];
+        $result   = ['A', 'Bish', 'C'];
 
-        $this->assertEquals(['A', 'Bish', 'C'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_can_use_multiple_rules()
     {
-        $rules = ['B' => 'Bish', 'C' => 'Cish'];
-        $data = ['A', 'B', 'C'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['A', 'B', 'C'];
+        $rules    = ['B' => 'Bish', 'C' => 'Cish'];
+        $result   = ['A', 'Bish', 'Cish'];
 
-        $this->assertEquals(['A', 'Bish', 'Cish'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_can_handle_more_matches_in_an_item()
     {
-        $rules = ['B' => 'Bish', 'C' => 'Cish'];
-        $data = ['A', 'BC', 'C'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['A', 'BC', 'C'];
+        $rules    = ['B' => 'Bish', 'C' => 'Cish'];
+        $result   = ['A', 'BishCish', 'Cish'];
 
-        $this->assertEquals(['A', 'BishCish', 'Cish'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_outputs_results_in_order_of_rules_not_matching_order()
     {
-        $rules = ['B' => 'Bish', 'C' => 'Cish'];
-        $data = ['A', 'BC', 'CB'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['A', 'BC', 'CB'];
+        $rules    = ['B' => 'Bish', 'C' => 'Cish'];
+        $result   = ['A', 'BishCish', 'BishCish'];
 
-        $this->assertEquals(['A', 'BishCish', 'BishCish'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_is_not_case_sensitive()
     {
-        $rules = ['B' => 'Bish', 'c' => 'Cish'];
-        $data = ['a', 'bc', 'CB'];
-        $bishNish = new BishNish($data, $rules);
+        $data     = ['a', 'bc', 'CB'];
+        $rules    = ['B' => 'Bish', 'c' => 'Cish'];
+        $result   = ['a', 'BishCish', 'BishCish'];
 
-        $this->assertEquals(['a', 'BishCish', 'BishCish'], $bishNish->generate());
+        $this->assertBishNished($data, $rules, $result);
     }
 
     /** @test */
     public function it_works_with_country_code_format()
     {
-        $rules = ['B' => 'Bish', 'N' => 'Nish'];
-        $data = ['aze', 'bhs', 'btn'];
+        $data   = ['aze', 'bhs', 'btn'];
+        $rules  = ['B' => 'Bish', 'N' => 'Nish'];
+        $result = ['aze', 'Bish', 'BishNish'];
+
+        $this->assertBishNished($data, $rules, $result);
+    }
+
+    /**
+     * Assert that a data array is BishNished correctly based on given rules.
+     *
+     * @param array $data
+     * @param array $rules
+     * @param array $result
+     */
+    protected function assertBishNished(array $data, array $rules, array $result)
+    {
         $bishNish = new BishNish($data, $rules);
 
-        $this->assertEquals(['aze', 'Bish', 'BishNish'], $bishNish->generate());
+        $this->assertEquals($result, $bishNish->generate());
     }
 }
