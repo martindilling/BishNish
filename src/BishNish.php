4260f121e126b3f24d74969532585ef53126d28f
Martin Dilling-Hansen <martindilling@gmail.com>
2015-03-01 15:01:46 +0100

    BishNish class refactored

--- src/BishNish.php
+++ src/BishNish.php
@@ -2,36 +2,103 @@
 
 class BishNish
 {
+    /**
+     * @var array
+     */
     protected $data;
+
+    /**
+     * @var array
+     */
     protected $rules;
 
+    /**
+     * @param array $data
+     * @param array $rules
+     */
     function __construct(array $data, array $rules)
     {
         $this->data  = $data;
         $this->rules = $rules;
     }
 
+    /**
+     * Generate an array where the rules are applied.
+     *
+     * @return array
+     */
     public function generate()
     {
-        if (empty($this->rules)) {
+        if ($this->isRulesEmpty()) {
             return $this->data;
         }
 
-        $result = array_map(function ($item) {
-            $replaceWith = [];
-            foreach ($this->rules as $match => $alternative) {
-                if (mb_strpos(mb_strtoupper($item), mb_strtoupper($match)) !== false) {
-                    $replaceWith[] = $alternative;
-                }
-            }
-
-            if (!empty($replaceWith)) {
-                return implode('', $replaceWith);
-            }
-
-            return $item;
-        }, $this->data);
+        $result = $this->applyRulesToArray($this->data);
 
         return $result;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isRulesEmpty()
+    {
+        return empty($this->rules);
+    }
+
+    /**
+     * @param array $dataArray
+     * @return array
+     */
+    protected function applyRulesToArray(array $dataArray)
+    {
+        return array_map(function ($item) {
+            return $this->getNewItemName($item);
+        }, $dataArray);
+    }
+
+    /**
+     * @param string $item
+     * @return string
+     */
+    function getNewItemName($item)
+    {
+        $matches = $this->getMatchesForItem($item);
+
+        if (empty($matches)) {
+            return $item;
+        }
+
+        return implode('', $matches);
+    }
+
+    /**
+     * @param string $item
+     * @return array
+     */
+    protected function getMatchesForItem($item)
+    {
+        $matches = [];
+
+        foreach ($this->rules as $match => $replacement) {
+            if ($this->isStringContaining($item, $match)) {
+                $matches[] = $replacement;
+            }
+        }
+
+        return $matches;
+    }
+
+    /**
+     * @param string $haystack
+     * @param string $needle
+     * @return bool
+     */
+    function isStringContaining($haystack, $needle)
+    {
+        $uppercaseHaystack = mb_strtoupper($haystack);
+        $uppercaseNeedle   = mb_strtoupper($needle);
+
+        return mb_strpos($uppercaseHaystack, $uppercaseNeedle) !== false;
     }
 }
